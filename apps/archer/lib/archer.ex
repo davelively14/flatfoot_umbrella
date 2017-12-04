9576b0cbfe06a9208710d31e6b6dd31608286f5d
Dave Lively <davelively14@gmail.com>
2017-12-03 19:22:19 -0500

    Removed boilerplate

--- apps/archer/lib/archer.ex
+++ apps/archer/lib/archer.ex
@@ -2,17 +2,4 @@ defmodule Archer do
   @moduledoc """
   Documentation for Archer.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Archer.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
